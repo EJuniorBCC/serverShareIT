2d3cfd8b44f6a3b08d75e18271752f1852e4bf45
Elias Junior <jnkjuniorhotmail.com>
2016-06-27 14:42:59 -0300

    Teste1

--- server/util/tokenVerification.js
+++ server/util/tokenVerification.js
@@ -3,9 +3,13 @@ module.exports = function(req, res, next) {
 	var token = '';
 	var config = require('../config');
 
+	console.log(req.params);
+
 	if (req.params.token) {
 		token = req.params.token
 	}
+
+	console.log(token);
 
 
 	jwt.verify(token, config.secret, function(err, data) {
